87b853db85810d064ed04e36fa66e21c034d61a1
Khaled Albhlal <khaledbhlal@outlook.com>
2020-06-30 19:39:39 +0300

    refactor: change variable name "def load_data"
     changed the variable name of (originally) "def load_data" to
     "def_all_data"

--- bikeshare.py
+++ bikeshare.py
@@ -47,7 +47,7 @@ def get_filters():
             return city, month, day
 
 
-def load_data(city, month, day):
+def load_all_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
 
@@ -178,7 +178,7 @@ return
 def main():
         while True:
             city, month, day = get_filters()
-            df = load_data(city, month, day)
+            df = load_all_data(city, month, day)
 
             time_stats(df)
             station_stats(df)
